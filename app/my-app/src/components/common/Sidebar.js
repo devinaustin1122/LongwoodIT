feffad1ddcd7557e3d5dd4880aaff301ff8c55b0
Devin Dyer <dyerda@Devins-MacBook-Air.local>
2021-02-08 21:33:25 -0500

    Sidebar updates.

--- app/my-app/src/components/common/Sidebar.js
+++ app/my-app/src/components/common/Sidebar.js
@@ -1,14 +1,37 @@
 import React from "react";
 import { connect } from "react-redux";
-import * as userActions from "../../redux/actions/userActions.js";
+import * as listActions from "../../redux/actions/listActions.js";
 import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 import { store } from "react-notifications-component";
 
 class Sidebar extends React.Component {
+  onComponentDidMount() {
+    this.props.actions.loadLists(this.props.user);
+  }
+
   render() {
-    return <div className="sidebar"></div>;
+    return (
+      <div className="sidebar">
+        {/* {this.props.lists.map((list) => (
+          <p key={list.name}>{list.name}</p>
+        ))} */}
+      </div>
+    );
   }
 }
 
-export default Sidebar;
+function mapStateToProps(state) {
+  return {
+    lists: state.lists.all,
+    user: state.users.active,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: bindActionCreators(listActions, dispatch),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Sidebar);
